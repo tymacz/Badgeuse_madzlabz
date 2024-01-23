139b7951c66cdf805221313e5dcc38071b3b2b9b
tymacz <maxence.rebours@outlook.fr>
2024-01-23 16:55:00 +0100

    nouvelle version de script.py

--- script.py
+++ script.py
@@ -7,6 +7,7 @@ import MFRC522
 import signal
 import mysql.connector
 import time
+import datetime
 
 continue_reading = True
 
@@ -28,46 +29,52 @@ def end_read(signal,frame):
 signal.signal(signal.SIGINT, end_read)
 MIFAREReader = MFRC522.MFRC522()
 
-print ("Passer le tag RFID a lire")
 
 while continue_reading:
-    
     # Detecter les tags
     (status,TagType) = MIFAREReader.MFRC522_Request(MIFAREReader.PICC_REQIDL)
 
     # Une carte est detectee
-    if status == MIFAREReader.MI_OK:
-        print ("Carte detectee")
-        
     # Recuperation UID
     (status,uid) = MIFAREReader.MFRC522_Anticoll()
 
     if status == MIFAREReader.MI_OK:
+        x = datetime.datetime.now()
         key_badge = str(uid[0])+"."+str(uid[1])+"."+str(uid[2])+"."+str(uid[3])
         sql = ("select arrive_matin,depart_matin,arrive_soir,depart_soir  from journee inner join employee on journee.id_badge = employee.id_badge where jour = %s and key_badge = %s")
-        cursor.execute(sql,("2024-01-22",key_badge))
+        cursor.execute(sql,(x.strftime("%Y-%m-%d"),key_badge))
         value = cursor.fetchall()
         key = [key_badge]
-        idSql = ("select id_badge from employee where key_badge = %s")
+        idSql = ("select id_badge ,nom , prenom from employee where key_badge = %s")
         cursor.execute(idSql,(key))
         id = cursor.fetchone()
-        if (len(value)==0):
-         sendSql= ("insert into journee (jour, arrive_matin,id_badge) values(%s,%s,%s)")
-         cursor.execute(sendSql,("2024-01-22","9:00",id[0]))
-         connection.commit()
-        value1 =value[0].count(None)
-        print ("UID de la carte : ",value1)
-        # Clee d authentification par defaut
-        key = [0xFF,0xFF,0xFF,0xFF,0xFF,0xFF]
-        # Selection du tag
-        MIFAREReader.MFRC522_SelectTag(uid)
-
-        # Authentification
-        status = MIFAREReader.MFRC522_Auth(MIFAREReader.PICC_AUTHENT1A, 8, key, uid)
-
-        if status == MIFAREReader.MI_OK:
-            MIFAREReader.MFRC522_Read(8)
-            MIFAREReader.MFRC522_StopCrypto1()
-        else:
-            print ("Erreur d\'Authentification")
+        try :
+         if (len(value[0])==4):
+          nonecpt = value.count(None)
+          match nonecpt :
+           case 4:
+            sendSql= ("insert into journee (jour, arrive_matin,id_badge) values(%s,%s,%s)")
+            cursor.execute(sendSql,(x.strftime("%Y-%m-%d"),x.strftime("%X"),id[0]))
+            connection.commit()
+            print ("Hi",id[1],id[2] )
+            print (value)
+           case 3:
+            sendSql=("update journee set depart_matin = %s where jour = %s and id_badge = %s")
+            cursor.execute(sendSql,(x.strftime("%X"),x.strftime("%Y-%m-%d"),id[0]))
+            connection.commit()
+            print ("Bye",id[1],id[2])
+           case 2: 
+            sendSql=("update journee set arrive_soir = %s where jour = %s and id_badge = %s")
+            cursor.execute(sendSql,(x.strftime("%X"),x.strftime("%Y-%m-%d"),id[0]))
+            connection.commit()
+            print ("Hi",id[1],id[2])
+           case 1 :
+            sendSql = ("update journee set depart_soir = %s where jour = %s and id_badge = %s")
+            cursor.execute(sendSql,(x.strftime("%X"),x.strftime("%Y-%m-%d"),id[0]))
+            connection.commit() 
+            print("Bye",id[1],id[2])
+           case 0 :
+            print ("Error : all clocking off are made")
+        except : 
+         print ("badge not recognized")
         time.sleep(5)
